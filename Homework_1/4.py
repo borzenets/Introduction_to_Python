4ab8ca7f875359749d6a571855d7152ade2aac6c
Andrey B <a.borzenets@gmail.com>
2023-02-18 17:22:31 +0300

    Added task 4 to homework 1

---
+++ Homework_1/4.py
@@ -0,0 +1,16 @@
+# Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек,
+# если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
+# Пример:
+# 3 2 4 -> yes
+# 3 2 1 -> no
+
+
+col_chocolate = int(input('Сколько долек в длинну: '))
+row_chocolate = int(input('Сколько долек в ширину: '))
+cell_quantity = int(input('Сколько долек вы хотите отломить одним действием: '))
+if (cell_quantity % col_chocolate == 0 and cell_quantity / col_chocolate < row_chocolate or
+    cell_quantity % row_chocolate == 0 and cell_quantity / row_chocolate < col_chocolate):
+
+    print('Вы можете это сделать, приятного аппетита!')
+else:
+    print('Вы это сделать не сможете.')
